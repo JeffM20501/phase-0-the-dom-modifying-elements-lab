f177bf93e9acf310a462a57655683376aef8a2f1
JeffM20501 <jeff.muna@student.moringaschool.com>
2025-09-04 21:38:28 +0300

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+const mainEl = document.getElementById('main')
+const newHeader = document.createElement("h1")
+newHeader.id='victory'
+newHeader.textContent='Jeff is the champion'
+main.remove()
